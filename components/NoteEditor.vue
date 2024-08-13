524761daa62df8a93f0e2fca7ddebe35d62a9ee1
Joao Oliveira <Joao.Oliveira7@br.ey.com>
2024-08-13 16:36:58 -0300

    Implementing delete confirmation popup

--- components/NoteEditor.vue
+++ components/NoteEditor.vue
@@ -30,7 +30,7 @@ export default Vue.extend({
   data() {
     return {
       currentNote: new Note(),
-      showConfirmDeletionPopup: true,
+      showConfirmDeletionPopup: false,
     };
   },
   watch: {
